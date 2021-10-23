8f3be3d227d71079d381416a1cd5ea6fa059a624
Rahul2289 <burrarahulgoud1999@gmail.com>
2021-10-23 19:44:27 +0530

    parse and stringify of Json

--- day-1/script.js
+++ day-1/script.js
@@ -19,3 +19,43 @@
     }
 */
 
+                        //JSON.parse()
+     
+      /*    When receiving data from a web server, the data is always a string.
+
+           Parse the data with JSON.parse(), and the data becomes a JavaScript object.*/
+
+                              //EXAMPLE
+
+   const  text = '{"name":"Rahul", "age":22, "city":"hyderabad"}'
+   let object   = JSON.parse(text);
+   console.log(`${object.name} is ${object.age} old,stay in ${object.city}` ) // OUTPUT:Rahul is 22 old,stay in hyderabad
+
+                        //JSON.stringify()
+    
+       /*      When sending data to a web server, the data has to be a string.
+
+              Convert a JavaScript object into a string with JSON.stringify().*/
+
+                            //EXAMPLE
+   
+   const obj = {name:"rahul",age:22,city:"hyderabad"}
+   const myJson =JSON.stringify(obj)
+   console.log(myJson)          //OUTPUT:{"name":"rahul","age":22,"city":"hyderabad"}
+   
+{
+                // Storing data:
+    const myObj = { name: "rahul", age: 22, city: "hyderabad" };
+    const myJSON = JSON.stringify(myObj);
+     localStorage.setItem("testJSON", myJSON);
+                    // Retrieving data:
+     let text = localStorage.getItem("testJSON");
+     let obj = JSON.parse(text);
+
+        console.log(obj.name)
+}
+                        
+                                        
+
+                                       
+
