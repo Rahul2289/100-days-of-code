286721caf05c70c4b76084a076b9a8fb4f867dec
Rahul2289 <burrarahulgoud1999@gmail.com>
2021-10-23 19:01:10 +0530

    json syntax and data types

---
+++ day-1/script.js
@@ -0,0 +1,21 @@
+                   //JSON
+     //    JSON stands for JavaScript Object Notation
+     //    JSON is a text format for storing and transporting data      
+     
+                   //syntax and Data types 
+                   
+  /*   {
+        "name":"Rahul",                                  -->String
+       
+        "age":22,                                        -->Number
+       
+        "address":{"country":"india","city":"hyderabad"},-->object
+
+        "activities":["playing","sleeping","eating"],     -->Array
+
+        "male":true,                                      -->Boolean
+
+        "handicap":null                                    -->null
+    }
+*/
+
